4432fa8c34a1fa03af7e02c68b987ac163008cef
Peter Yang <namkyux@gmail.com>
2017-05-23 21:30:50 -0500

    update

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,4 +103,42 @@ describe('GET /todos/:id', () => {
 	});
 });
 
+describe('DELETE /todos/:id', () => {
+	it('should remove a todo', (done) => {
+		var hexId = seedTodo[1]._id.toHexString();
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId)
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.findById(hexId).then((todo) => {
+					expect(todo).toNotExist();
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectID().toHexString();
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if object is invalid', (done) => {
+		var id = 'asdfasdfas';
+		request(app)
+			.delete(`/todos/${id}`)
+			.expect(404)
+			.end(done);
+	});
+});
+
 // it('should return a 404 if todo not found')
